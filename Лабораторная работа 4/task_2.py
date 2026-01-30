2083113dd851b7175795926944d5b54e0d2350fc
Malokonomin <malokonomin1@gmail.com>
2026-01-30 10:37:36 +0300

    Add files via upload

--- Лабораторная работа 4/task_2.py
+++ Лабораторная работа 4/task_2.py
@@ -1,14 +1,21 @@
 # TODO импортировать необходимые молули
-
+import csv
+import json
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    ...  # TODO считать содержимое csv файла
+    with open(INPUT_FILENAME) as f:
+        csv_data = csv.DictReader(f)
+        rows = list(csv_data)
+    # TODO считать содержимое csv файла
 
-    ...  # TODO Сериализовать в файл с отступами равными 4
+    with open(OUTPUT_FILENAME, "w") as f:
+       json.dump(rows,f)
+
+    # TODO Сериализовать в файл с отступами равными 4
 
 
 if __name__ == '__main__':
